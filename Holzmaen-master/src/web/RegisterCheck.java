7ac4fa8d46c96dd349ee1e14ad0a60d26bb328cd
Nosabua2000 <Nosabua123>
2020-12-14 13:36:58 +0100

    added new library and ReadArtikel.java and Main.jsp

--- Holzmaen-master/src/web/RegisterCheck.java
+++ Holzmaen-master/src/web/RegisterCheck.java
@@ -1,5 +1,6 @@
 package web;
 
+import com.google.gson.Gson;
 import Übung02.model.Benutzer;
 import Übung02.model.DBManager;
 
@@ -11,8 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @WebServlet(name = "RegisterCheck")
 public class RegisterCheck extends HttpServlet {
@@ -94,4 +97,59 @@ public class RegisterCheck extends HttpServlet {
 
     }
 
+    /**
+     * Servlet implementation class ReadArtikel
+     */
+    @WebServlet("/ReadArtikel")
+    public static class ReadArtikel extends HttpServlet {
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * @see HttpServlet#HttpServlet()
+         */
+        public ReadArtikel() {
+            super();
+            // TODO Auto-generated constructor stub
+        }
+
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            // TODO Auto-generated method stub
+        }
+
+        /**
+         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            String strId = request.getParameter("themenId");
+            int themengebietid = Integer.parseInt(strId);
+            DBManager m = DBManager.getInstance();
+            Connection con = null;
+            ArrayList<Artikel> liste  = new ArrayList<>();
+            try {
+                con = m.getConnection();
+                liste = m.leseArtikelVonThemenebiet(con, themengebietid);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (con != null)
+                    try {
+                        m.releaseConnection(con);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+            }
+            Gson gson = new Gson();
+            String jsonResult = gson.toJson(liste);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter out = response.getWriter();
+            out.append(jsonResult);
+            out.flush();
+        }
+
+
+    }
 }
